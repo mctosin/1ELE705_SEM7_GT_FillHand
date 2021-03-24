a47ca12067d055e5ee5964ab7f8861bb36c5bc9e
mctosin <mctosin@gmail.com>
2021-03-24 13:06:11 -0300

    Primeiro commit.

---
+++ pch.cpp
@@ -0,0 +1,14 @@
+//
+// pch.cpp
+// Include the standard header and generate the precompiled header.
+//
+
+#include "pch.h"
+#include <stddef.h>
+
+unsigned int fillHandExt(Card* const wDeck, Card* const wHand, const unsigned int Ncards) {
+
+
+
+    return -1;
+}
